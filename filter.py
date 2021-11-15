7a1e890b84d6430567fb933f57c97da88e0633de
Никита Топлин АТ-29 <ntoplin@mail.ru>
2021-11-15 23:34:41 +0500

    Toplin_Nikita_AT-29_Step2

--- filter.py
+++ filter.py
@@ -1,29 +1,52 @@
 from PIL import Image
 import numpy as np
+
+
+def get_size_and_grad(height_len, width_len):
+    correct = True
+    flag = 0
+    while correct:
+        if flag > 0:
+            print("размеру мозаики должен быть делителем для ширины и высоты изображения, "
+                  "а градация серого должна быть меньше 128")
+        print('Введите размер мозаики и градацию серого через  пробел: ')
+        flag += 1
+        array = input().split(' ')
+        array = [int(x) for x in array]
+        if height_len % array[0] == 0 and width_len % array[0] == 0 and array[1] < 128:
+            correct = False
+            print('Данные корректы, ищите результат в папке.')
+    return(array[0], array[1])
+
+
+def search_grey(i, j, array, size, grad):
+    total_grey = 0
+    for row in range(i, i + size):
+        for column in range(j, j + size):
+            red = array[row][column][0]
+            blue = array[row][column][1]
+            green = array[row][column][2]
+            grey = (int(red) + int(blue) + int(green)) / 3
+            total_grey += grey
+    total_grey = int(total_grey // (size * size))
+    for row in range(i, i + size):
+        for column in range(j, j + size):
+            array[row][column][0] = int(total_grey // grad) * grad
+            array[row][column][1] = int(total_grey // grad) * grad
+            array[row][column][2] = int(total_grey // grad) * grad
+
+
 img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
+pixel_array = np.array(img)
+len_height = len(pixel_array)
+len_width = len(pixel_array[1])
+(size, grad) = get_size_and_grad(len_height, len_width)
 i = 0
-while i < a:
+while i < len_height:
     j = 0
-    while j < a1:
-        s = 0
-        for s1 in range(i, i + 10):
-            for s2 in range(j, j + 10):
-                n1 = arr[s1][s2][0]
-                n2 = arr[s1][s2][1]
-                n3 = arr[s1][s2][2]
-                M = (int(n1) + int(n2) + int(n3)) / 3
-                s += M
-                M = 0
-        s = int(s // 100)
-        for s1 in range(i, i + 10):
-            for s2 in range(j, j + 10):
-                arr[s1][s2][0] = int(s // 50) * 50
-                arr[s1][s2][1] = int(s // 50) * 50
-                arr[s1][s2][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
+    while j < len_width:
+        search_grey(i, j, pixel_array, size, grad)
+        j = j + size
+    i = i + size
+res = Image.fromarray(pixel_array)
 res.save('res.jpg')
